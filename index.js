53ea1e85f45b77ba18b50b364c83090839400d1b
eldis.izeirovski <zeld.exe.dev@gmail.com>
2024-04-10 09:28:59 +0200

    user created

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+
 const { Client, Events, GatewayIntentBits, Collection} = require('discord.js');
 const { token } = require('./config.json');
 const path = require("node:path");
 const fs = require("node:fs");
 
-const client = new Client({intents: [GatewayIntentBits.Guild, GatewayIntentBits.Client]});
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+    ]
+});
 
 client.once(Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}.`);
 });
 
 client.commands = new Collection();
-
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
-for (const folder in commandFolders) {
+for (const folder of commandFolders) {
     const commandsPath = path.join(foldersPath, folder);
     const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
     for (const file of commandFiles) {
@@ -23,12 +27,10 @@ for (const folder in commandFolders) {
         if ('data' in command && 'execute' in command) {
             client.commands.set(command.data.name, command);
         } else {
-            console.log(`[WARNING] The command ${filePath} is missing a required "data" or "execute" property.`);
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
         }
     }
 }
 
 
-client.login(token).then(() => {
-    console.log(`token: ${token}`);
-});
+client.login(token);
